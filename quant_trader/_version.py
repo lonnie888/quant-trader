c264b3919793e12f27a2d8c20014c33504bf5dd7
lonnie <lonnie@debian>
2026-07-13 11:38:08 +0800

    v0.3.10

--- quant_trader/_version.py
+++ quant_trader/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.3.9"
-__release__ = "v0.3.9"
+__version__ = "0.3.10"
+__release__ = "v0.3.10"
 __description__ = "Quant Trader - Binance USDT-margined perpetual futures paper-trading system"
