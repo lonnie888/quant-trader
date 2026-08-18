ceaaea0f30a68688959533e81ed79824089287db
lonnie <lonnie@debian>
2026-08-18 22:33:01 +0800

    chore: bump version to v0.6.0

--- quant_trader/_version.py
+++ quant_trader/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.5.0"
-__release__ = "v0.5.0"
-__description__ = "Quant Trader - Binance USDT-margined perpetual futures paper-trading system"/usr/bin/bash: line 7: /tmp/w/hermes-cwd-f74847fbf4e3.txt: No such file or directory
+__version__ = "0.6.0"
+__release__ = "v0.6.0"
+__description__ = "Quant Trader - Binance USDT-margined perpetual futures paper-trading system"
